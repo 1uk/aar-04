76f65b556cf2dfb7703c0808ee208bae031593c6
Lukas Kitsche <lu.kitsche@gmail.com>
2014-12-24 15:33:03 +0100

    ir test no output.

--- infrarot/infrarot.ino
+++ infrarot/infrarot.ino
@@ -1,1 +1,18 @@
-#include <../Arduino-IRremote/IRremote.h>
+#include <libraries/Arduino-IRremote/IRremote.h>
+IRrecv irrecv(2);  // pin
+decode_results results;
+
+void setup()
+{
+    Serial.begin(9600);
+    irrecv.enableIRIn(); // Start the receiver
+    Serial.println("initialized");
+}
+
+void loop() {
+    if (irrecv.decode(&results)) {
+        Serial.print("received: ");
+        Serial.println(results.value, HEX);
+        irrecv.resume(); // Continue receiving
+    }
+}
